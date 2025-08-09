716e9ef530ac045c01c00df54d198a6297666f59
Denys Andrukhovskyi <andruhovski@outlook.com>
2025-08-09 23:45:07 +0200

    scripts for generating structure

---
+++ generate_bash.sh
@@ -0,0 +1,26 @@
+./new-multilang.sh chapter fmed/_index.md
+./new-multilang.sh chapter flaw/_index.md
+./new-multilang.sh chapter fphil/_index.md
+./new-multilang.sh chapter fns/_index.md
+./new-multilang.sh chapter fedu/_index.md
+./new-multilang.sh chapter fpharm/_index.md
+./new-multilang.sh chapter fsport/_index.md
+./new-multilang.sh chapter jfmed/_index.md
+./new-multilang.sh chapter fmph/_index.md
+./new-multilang.sh chapter frcth/_index.md
+./new-multilang.sh chapter fevth/_index.md
+./new-multilang.sh chapter fphil/_index.md
+./new-multilang.sh chapter fm/_index.md
+./new-multilang.sh chapter fses/_index.md
+
+./new-multilang.sh chapter ubytovanie/_index.md
+
+./new-multilang.sh chapter ubytovanie/mlyny/_index.md
+./new-multilang.sh chapter ubytovanie/druzba/_index.md
+./new-multilang.sh chapter ubytovanie/general/_index.md
+
+./new-multilang.sh chapter general/_index.md
+
+./new-multilang.sh default about.md
+./new-multilang.sh home _index.md
+./new-multilang.sh default faq.md
